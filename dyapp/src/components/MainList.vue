455317debb1943d88c8885e515a0e4408ea4ad80
kang-dayeon <dksldiss89@gmail.com>
2022-08-12 16:22:02 +0900

    컴포넌트 만들어보기,,좀 더 공부 필요

---
+++ dyapp/src/components/MainList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list-content">
+    <ul class="list">
+        <li v-bind:class="{ noneList : noneList }"><a href="{{ listLink }}">{{ listItem }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MainList',
+    data: {
+        noneList : true,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
