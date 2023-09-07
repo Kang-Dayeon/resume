c8206579f7ba8df4d65881dd5d91be83eabca2e7
kang-dayeon <dksldiss89@gmail.com>
2023-09-08 00:45:22 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-
-
 const controller = new ScrollMagic.Controller();
 
-const animateElem = [".animate_01", ".animate_02", ".animate_03"];
-const triggerElem = [".trigger_01", ".trigger_02", ".trigger_03"];
+const animateElem = [".animate_01", ".animate_02"];
+const triggerElem = [".trigger_01", ".trigger_02"];
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 2; i++) {
   let currentAnimateElem = animateElem[i];
   let currentTriggerElem = triggerElem[i];
 
@@ -13,7 +11,7 @@ for (let i = 0; i < 3; i++) {
 
   let scene = new ScrollMagic.Scene({
     triggerElement: currentTriggerElem,
-    duration: "1800px"
+    duration: "1100px"
   })
     .setTween(timeline)
     .addTo(controller)
@@ -47,4 +45,66 @@ for (let i = 0; i < 3; i++) {
   })
 
 }
+
+//input event 
+function inputValue(input, message, user) {
+  input.keydown(e => {
+    if (e.keyCode == 13) {
+      let inputValue = input.val();
+      user.text(inputValue);
+      input.removeClass('active');
+      message.addClass('active');
+    }
+  });
+};
+
+// top scroll
+function topScolling(topBtn) {
+  topBtn.click(() => {
+    $('html,body').animate({ scrollTop: 0 }, 500);
+  });
+};
+
+// mouse event
+function mouseMoving(el) {
+  el.mousemove(e => {
+    $('.cursor').css('top', e.clientY + 100);
+    $('.cursor').css('left', e.clientX);
+  });
+};
+
+// TweenMax
+// function circleAnimation(el, circle) {
+//   el.click(e => {
+//     let tl = new TimelineLite;
+//     tl.to(circle, 1.5, {
+//       rotation: 180,
+//       x: e.clientX,
+//       y: e.clientY + 100,
+//       width: 50,
+//       height: 50,
+//       ease: Back.easeOut.config(1)
+//     }).to(circle, 2, {
+//       rotation: 360,
+//       width: 300,
+//       height: 300,
+//       ease: Back.easeOut.config(1.7),
+//     });
+//   });
+// };
+
+
+
+
+$(document).ready(function () {
+  // aos적용
+  AOS.init();
+  // event function
+  inputValue($('.name-input'), $('.message'), $('.user-name'));
+  topScolling($('.top'));
+  mouseMoving($('#mouseStart'));
+  // circleAnimation($('#mouseStart'), '.circle-click')
+});
+
+
 
